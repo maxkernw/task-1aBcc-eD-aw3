34bcebd7ece50a5b6b2f815c4e3afd2328454098
maxkernw <maxkernw@gmail.com>
2018-09-09 21:24:50 +0200

    nested

--- src/app/components/subreddit/subreddit.component.ts
+++ src/app/components/subreddit/subreddit.component.ts
@@ -7,7 +7,6 @@ import { Data } from '../../services/storage.service';
 const DEFAULT_LIMIT = 10;
 const DEFAULT_SUBREDDIT = "sweden"
 
-
 @Component({
   selector: 'app-subreddit',
   templateUrl: './subreddit.component.html',
@@ -15,44 +14,45 @@ const DEFAULT_SUBREDDIT = "sweden"
 })
 export class SubredditComponent implements OnInit {
   data: Subreddit[];
-  query: string = DEFAULT_SUBREDDIT;
   after: string;
   before: string;
-  limit: number = DEFAULT_LIMIT;
   title: string;
   loading: boolean;
   error: boolean = false;
+  
+  query: string = DEFAULT_SUBREDDIT;
+  limit: number = DEFAULT_LIMIT;
 
   constructor(
     private redditService: RedditService, private queryStorage: Data<{ query: string }>) { }
 
   ngOnInit(): void {
     this.query = this.queryStorage.storage ? this.queryStorage.storage.query : DEFAULT_SUBREDDIT
-    this.getData(this.query, DEFAULT_LIMIT)
+    this.getData()
   }
 
   update(value: string): void {
     this.query = value;
     this.queryStorage.storage = { query: value };
-    this.getData(value, this.limit)
+    this.getData()
   }
 
   next(): void {
-    this.getData(this.query, this.limit, `&after=${this.after}`)
+    this.getData(`&after=${this.after}`)
   }
 
   prev(): void {
-    this.getData(this.query, this.limit, `&before=${this.before}`)
+    this.getData(`&before=${this.before}`)
   }
 
   limitresults(limit): void {
     this.limit = limit;
-    this.getData(this.query, limit)
+    this.getData()
   }
 
-  private getData(query: string, limit: number, pagination?: string): void {
+  private getData(pagination?: string): void {
     this.loading = true;
-    const url = `${query}.json?limit=${limit}&count=100${pagination}`
+    const url = `${this.query}.json?limit=${this.limit}&count=100${pagination}`
 
     this.redditService.get<RedditResponse<Subreddit>>(url).subscribe((response: RedditResponse<Subreddit>) => {
 
@@ -60,7 +60,7 @@ export class SubredditComponent implements OnInit {
       this.data = children
       this.after = after;
       this.before = before;
-      this.title = `/r/${query}`
+      this.title = `/r/${this.query}`
 
       this.loading = false;
       this.error = false;
